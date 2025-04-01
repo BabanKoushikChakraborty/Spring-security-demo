747d6e915e933bbec664d4b52736ab788ab8b1ae
Admin <Admin@host.docker.internal>
2025-04-02 04:31:41 +0530

    3

--- src/main/java/com/example/demo/service/JwtService.java
+++ src/main/java/com/example/demo/service/JwtService.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
-import java.util.Date;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
--- src/main/java/com/example/demo/service/MyUserDetailsService.java
+++ src/main/java/com/example/demo/service/MyUserDetailsService.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.dao.UserRepo;
 import com.example.demo.model.User;
 import com.example.demo.model.UserPrincipal;
